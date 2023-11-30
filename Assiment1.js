c3a59dfc6046ba3ed3d1676a19c250ff19787872
nghiemhv123 <143272812+nghiemhv123@users.noreply.github.com>
2023-11-30 15:21:43 +0700

    ASM1-JS

---
+++ Assiment1.js
@@ -0,0 +1,178 @@
+"use strict";
+
+// 1. Bắt sự kiện Click vào nút "Submit" 
+const submitBtn = document.getElementById("submit-btn");
+const healthyBtn = document.getElementById("healthy-btn");
+const idInput = document.getElementById("input-id");
+const nameInput = document.getElementById("input-name");
+const ageInput = document.getElementById("input-age");
+const typeInput = document.getElementById("input-type");
+const weightInput = document.getElementById("input-weight");
+const lengthInput = document.getElementById("input-length");
+const colorInput = document.getElementById("input-color-1");
+const breedInput = document.getElementById("input-breed");
+const vaccinatedInput = document.getElementById("input-vaccinated");
+const dewormedInput = document.getElementById("input-dewormed");
+const sterilizedInput = document.getElementById("input-sterilized");
+const bmiBtn = document.getElementById("indexbmi-btn");
+
+// 2. Lấy được dữ liệu từ các Input Form 
+const data = {
+    Id: idInput.value,
+    Name: nameInput.value,
+    Age: parseInt(ageInput.value),
+    Type: typeInput.value,
+    Weight: parseInt(weightInput.value),
+    Length: parseInt(lengthInput.value),
+    Color: colorInput.value,
+    Breed: breedInput.value,
+    Vaccinated: vaccinatedInput.checked,
+    Dewormed: dewormedInput.checked,
+    Sterilized: sterilizedInput.checked,
+    bmi: '?',
+    Date: new Date(),
+};
+// đổi định dạng ngày giờ 
+const result2 = data.Date.toLocaleDateString('vi-VN', {
+    hour12: false,
+});
+console.log(result2);
+console.log(data);
+
+const petArr = [];
+// 3. Validate dữ liệu hợp lệ 
+// gán onclick ="validate" ở thẻ button-sumit 
+function Validate() {
+    //thông báo ID pet trùng nhau
+    for (let i = 0; i < petArr.length; i++) {
+        if (idInput.value === petArr[i].Id) {
+            alert("ID must be unique!");
+        }
+    }
+    if (idInput.value == "") {
+        alert("Please input for ID!");
+    } else if (nameInput.value == "") {
+        alert("Please input for Name !");
+    } else if (ageInput.value == "" || ageInput.value > 15) {
+        alert("Age must be between 1 and 15!");
+    } else if (typeInput.value == "Select Type") {
+        alert("Please select Type!");
+    } else if (weightInput.value == '' || weightInput.value > 15) {
+        alert("Weight must be between 1 and 15!");
+    } else if (lengthInput.value == "" || lengthInput.value > 100) {
+        alert("Length must be between 1 and 100!");
+    } else if (breedInput.value == "Select Breed") {
+        alert("Please select Breed!");
+    } else {
+        const data = {
+            Id: idInput.value,
+            Name: nameInput.value,
+            Age: parseInt(ageInput.value),
+            Type: typeInput.value,
+            Weight: parseInt(weightInput.value),
+            Length: parseInt(lengthInput.value),
+            Color: colorInput.value,
+            Breed: breedInput.value,
+            Vaccinated: vaccinatedInput.checked,
+            Dewormed: dewormedInput.checked,
+            Sterilized: sterilizedInput.checked,
+            bmi: '?',
+            Date: new Date(),
+        };
+        console.log(data);
+        petArr.push(data);
+        clearInput(petArr);
+        renderTableData(petArr);
+    }
+
+}
+// 45. Hiển thị danh sách thú cưng 
+function renderTableData(petArr) {
+    const tableBodyEl = document.getElementById('tbody');
+    let row = "";
+    tableBodyEl.innerHTML = "";
+    for (let i = 0; i < petArr.length; i++) {
+        row += `<tr>
+    <th >${petArr[i].Id}</th>
+    <td >${petArr[i].Name}</td>
+    <td >${petArr[i].Age}</td>
+    <td >${petArr[i].Type}</td>
+    <td >${petArr[i].Weight} kg</td>
+    <td >${petArr[i].Length} cm</td>
+    <td >${petArr[i].Breed}</td>
+    <td>
+    <i class="bi bi-square-fill" style="color: ${petArr[i].Color}"></i>
+    </td>
+    <td >${petArr[i].Vaccinated ? "<i class='bi bi-check-circle-fill'></i>" : "<i class='bi bi-x-circle-fill'></i>"}</td>
+    <td >${petArr[i].Dewormed ? "<i class='bi bi-check-circle-fill'></i>" : "<i class='bi bi-x-circle-fill'></i>"}</td>
+    <td >${petArr[i].Sterilized ? "<i class='bi bi-check-circle-fill'></i>" : "<i class='bi bi-x-circle-fill'></i>"}</td>
+    <td >${petArr[i].bmi}</td>
+    <td style="color:blue;" >${petArr[i].Date.toLocaleDateString('vi-VN', {
+            hour12: false,
+        })}</td>
+    <td><button type="button" class="btn btn-danger"  style="background-color: red;" onclick="deletePet('${petArr[i].Id}')">Delete</button>
+    </td>
+</tr>`
+    }
+    //sử dụng điều kiện tam cấp cho Vaccinated,Dewormed,Sterilized : chú ý class='bi bi-x-circle-fill'  class sử dụng (')
+    tableBodyEl.innerHTML = row;
+}
+
+// 6. Xóa các dữ liệu vừa nhập trên Form 
+
+const clearInput = () => {
+    idInput.value = "";
+    nameInput.value = "";
+    ageInput.value = "";
+    typeInput.value = "Select Type";
+    weightInput.value = "";
+    lengthInput.value = "";
+    colorInput.value = "Select color";
+    breedInput.value = "Select Breed";
+    vaccinatedInput.checked = "";
+    dewormedInput.checked = "";
+    sterilizedInput.checked = "";
+}
+
+// 7. Xóa một thú cưng 
+
+const deletePet = (petId) => {
+    // Confirm before deletePet
+    if (confirm('Are you Delete?')) {
+        for (let i = 0; i < petArr.length; i++) {
+            if (petArr[i].Id == petId) {
+                petArr.splice(i, 1);
+                //splice i là vị trí của phần tử cần xóa,1 là số lượng phần tử cần xóa.
+            }
+        }
+        renderTableData(petArr)
+    }
+}
+//8. Hiển thị các thú cưng khỏe mạnh
+
+let healthyCheck = false;
+function healthy() {
+    const healthyPet = petArr.filter((pet) => pet.Vaccinated === true && pet.Dewormed == true && pet.Sterilized == true);
+    console.log(healthyPet);
+    if (!healthyCheck) {
+        renderTableData(healthyPet);
+        healthyBtn.textContent = 'Show All Pet';
+    } else {
+        renderTableData(petArr)
+        healthyBtn.textContent = 'Show Healthy Pet';
+    }
+    healthyCheck = !healthyCheck
+
+}
+
+//9. (Nâng cao) Tính toán chỉ số BMI
+
+const indexBMI = () => {
+    for (let i = 0; i < petArr.length; i++) {
+        petArr[i].bmi = petArr[i].Type === 'Dog'
+            ? ((petArr[i].Weight * 703 / petArr[i].Length ** 2).toFixed(2))
+            : ((petArr[i].Weight * 886 / petArr[i].Length ** 2).toFixed(2));
+        //toFixed(x)làm tròn một số đến chữ số thập phân thứ x
+    }
+    renderTableData(petArr)
+}
